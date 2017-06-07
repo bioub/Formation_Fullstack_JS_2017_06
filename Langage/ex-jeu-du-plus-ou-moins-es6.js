9778af2bfe26cf30a97d7681d0d68bd0f59a78e7
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-06-07 10:38:36 +0100

    Correction ES6

--- Langage/ex-jeu-du-plus-ou-moins-es6.js
+++ Langage/ex-jeu-du-plus-ou-moins-es6.js
@@ -1,78 +1,82 @@
+'use strict';
+
 const readline = require('readline');
 
 // 1 - Ecrire les 4 fonctions random sous la forme :
 // const maFonction = function() {
 //
 // };
-function getRandom() {
+const getRandom = function() {
     return Math.random();
-}
+};
 
-function getRandomArbitrary(min, max) {
+const getRandomArbitrary = function(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-function getRandomInt(min, max) {
+const getRandomInt = function(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-function getRandomIntInclusive(min, max) {
+const getRandomIntInclusive = function(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 // 2 - Transformer Jeu en class
-var Jeu = function(options) {
-    // 3 - Créer l'objet vide en valeur par défaut de la fonction constructeur
-    options = options || {};
-    // 4 - Remplacer tous les var par let ou const
-    var min = options.min || 0;
-    var max = options.max || 100;
-    this._entierAlea = getRandomIntInclusive(min, max);
-    this._essais = [];
-    this._rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-};
-
-Jeu.prototype.jouer = function() {
-    if (this._essais.length) {
-        // 5 - Template String
-        console.log('Vous avez déjà joué : ' + this._essais.join(', '));
+class Jeu {
+    constructor({min = 0, max = 100} = {}) {
+        // 3 - Créer l'objet vide en valeur par défaut de la fonction constructeur
+        // options = options || {};
+        // 4 - Remplacer tous les var par let ou const
+        // const min = options.min || 0;
+        // const max = options.max || 100;
+        this._entierAlea = getRandomIntInclusive(min, max);
+        this._essais = [];
+        this._rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
     }
 
-    this._rl.question('Quel est le nombre ? ',  answer => {
-
-        // 6 - Utiliser parseInt et isNaN sur l'objet Number
-        var entierSaisi = parseInt(answer);
-
-        if (isNaN(entierSaisi)) {
-            console.log('Erreur : il faut saisir un nombre');
-            return this.jouer();
+    jouer() {
+        if (this._essais.length) {
+            // 5 - Template String
+            console.log(`Vous avez déjà joué : ${this._essais.join(', ')}`);
         }
 
-        this._essais.push(entierSaisi);
+        this._rl.question('Quel est le nombre ? ',  answer => {
 
-        if (entierSaisi < this._entierAlea) {
-            console.log('Trop petit');
-            return this.jouer();
-        }
+            // 6 - Utiliser parseInt et isNaN sur l'objet Number
+            const entierSaisi = Number.parseInt(answer);
 
-        if (entierSaisi > this._entierAlea) {
-            console.log('Trop grand');
-            return this.jouer();
-        }
+            if (Number.isNaN(entierSaisi)) {
+                console.log('Erreur : il faut saisir un nombre');
+                return this.jouer();
+            }
 
-        console.log('Gagné !');
-        this._rl.close();
-    });
-};
+            this._essais.push(entierSaisi);
 
-var jeu = new Jeu({
+            if (entierSaisi < this._entierAlea) {
+                console.log('Trop petit');
+                return this.jouer();
+            }
+
+            if (entierSaisi > this._entierAlea) {
+                console.log('Trop grand');
+                return this.jouer();
+            }
+
+            console.log('Gagné !');
+            this._rl.close();
+        });
+    }
+}
+
+const jeu = new Jeu({
     min: 10,
     max: 20,
 });
