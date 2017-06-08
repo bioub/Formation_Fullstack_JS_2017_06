892c622b1ac73370fc44e6554296dcf8fa9db615
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-06-08 11:32:08 +0100

    Début de AddressBook

---
+++ AddressBook/server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const routesContacts = require('./routes/contacts');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Middleware de log
+app.use(morgan('dev'));
+
+// Routes
+app.use('/api/contacts', routesContacts);
+
+// Middleware 404 de l'API REST
+app.use('/api', (req, res, next) => {
+    res.statusCode = 404;
+    res.json({
+        message: 'Not found',
+    });
+});
+
+app.listen(port, () => {
+   console.log(`App started on port ${port}`);
+});
---
+++ AddressBook/server/routes/contacts.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Administrateur on 08/06/2017.
+ */
