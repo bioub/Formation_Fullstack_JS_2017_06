9256c4a4ca2c2ba66105fbaa01865e9cd944694e
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-06-09 14:42:02 +0100

    Enoncé Angular

--- AddressBook/client/app/contact-list/contact-list.controller.js
+++ AddressBook/client/app/contact-list/contact-list.controller.js
@@ -9,4 +9,23 @@
                $scope.contacts = res.data;
            });
     });
+
+    /*
+    1 - Sur ContactListCtrl :
+    Créer une méthode supprimer avec en param d'entrée
+    le contact à supprimer (supprimer(contact))
+    Dans le contact, l'id : contact._id
+    Faire une requete DELETE vers /api/contacts/4J534D43CI5H
+    Dans le callback, soit raffraichir la liste
+    Soit supprimer le contact du tableau
+    2 - Créer la page ContactShow
+    - Créer le dossier avec controller + template
+    - Créer la route (/contact/show/:id)
+    - Insérer la balise script et enregistrer le module dans app.module
+    - Au chargement du controller envoyer la requete GET vers
+     /api/contacts/4J534D43CI5H
+     - Pour récupérer l'ObjectId on peut utiliser $state.params.id
+     - Créer le lien avec ui-sref="contact-show({id: '4GS53JG'})
+
+     */
 }());
